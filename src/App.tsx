8fe7d793c840131afdd81650c0f4b1a6fc58956d
tookuyam <tookuyam@student.42tokyo.jp>
2024-05-26 14:23:03 +0900

    delete: components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-import { useState } from 'react';
-import TodoList from './js/components/TodoListElement';
-import { TodoListTable } from './js/model/TodoListTable';
 import './style.css';
 function App() {
-
-  const table = new TodoListTable('todo-list');
-  const [todoDatas, setTodoDatas] = useState(table.load());
-
-  const appendButton = () =>
-    setTodoDatas([...todoDatas, {}]);
-
   return (
     <div>
-      <button onClick={appendButton}>＋</button>
-      <TodoList todoDatas={todoDatas}></TodoList>
     </div>
   )
 }
--- src/js/components/TodoCard.tsx
+++
@@ -1,9 +0,0 @@
-import { TodoData } from "../model/TodoData";
-
-
-export default function TodoCard(props: { title?: string }) {
-    const todo = new TodoData(props);
-    return <>
-        <div className="w-1/2 h-1/3 mx-auto rounded overflow-hidden shadow-lg">{todo.title}</div>
-    </>
-}
