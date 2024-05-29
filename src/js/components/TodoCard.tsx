64c0edd4a5ee02caa7d64b9df0ab888f7bbee01b
tookuyam <tookuyam@student.42tokyo.jp>
2024-05-29 21:58:23 +0900

    add: focus on todo when click todo area

--- src/js/components/TodoCard.tsx
+++ src/js/components/TodoCard.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { TodoDataFormat } from "../model/TodoData";
 import { Card } from "./Card";
 
@@ -7,11 +8,15 @@ export default function TodoCard({ todo, updateTitle, deleteTodo }: {
     updateTitle: (event: React.ChangeEvent<HTMLInputElement>) => void,
     deleteTodo: () => void
 }) {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const focusInputField = () => {
+        inputRef?.current?.focus();
+    }
     return (
-        <div className="todo-card-container">
+        <div className="todo-card-container" onClick={focusInputField}>
             <Card>
                 <div className="todo-card-content">
-                    <input className="todo-title"
+                    <input className="todo-title" ref={inputRef}
                         type="text" value={todo.title} onChange={updateTitle} />
                     <button className="todo-delete-button"
                         onClick={deleteTodo}>
